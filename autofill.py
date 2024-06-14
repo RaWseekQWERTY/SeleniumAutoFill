2958858bc06d20ed8046e1701ecc1944cf0eaf16
rasik <rasikkayasthav@gmail.com>
2024-06-14 17:47:50 +0545

    .env to protect credintals

--- autofill.py
+++ autofill.py
@@ -1,13 +1,25 @@
+import os
+from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from pathlib import Path
 
+load_dotenv()
+#declarations
 debugging = True
+firstName = os.getenv("FIRST_NAME")
+lastName = os.getenv("LAST_NAME")
+email=os.getenv("EMAIL")
+phNo=os.getenv("PH_NO")
+password=os.getenv("PASSWORD")
+username=os.getenv("USERNAME")
+#driver path
 script_dir = Path(__file__).resolve().parent
 diver_path = script_dir.joinpath("chromedriver-linux64","chromedriver")
 service = Service(diver_path)
 chrome_options= Options()
+
 #keeps the chrome open until operation is completed
 if debugging:
     chrome_options.add_experimental_option("detach", True)
