c867b89f59d64863e77acb283ece3ba3f66b10d8
rasik <rasikkayasthav@gmail.com>
2024-06-14 17:05:48 +0545

    connecting chrome driver

--- autofill.py
+++ autofill.py
@@ -1,6 +1,22 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from pathlib import Path
 
-driver = webdriver.Chrome()
+debugging = True
+script_dir = Path(__file__).resolve().parent
+diver_path = script_dir.joinpath("chromedriver-linux64","chromedriver")
+service = Service(diver_path)
+chrome_options= Options()
+#keeps the chrome open until operation is completed
+if debugging:
+    chrome_options.add_experimental_option("detach", True)
+    #input("Enter to exit")from selenium.webdriver.chrome.service import Service
+else:
+    chrome_options.add_argument("--headless")
 
-driver.get("http://selenium.dev")
-driver,quit()
+driver = webdriver.Chrome(service=service, options=chrome_options)
+driver.get("https://www.google.com")
+
+if not debugging:
+    driver.quit()
