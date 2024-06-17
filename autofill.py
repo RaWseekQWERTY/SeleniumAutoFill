eacb59c1f2b25f648e39d91b15cebb35f1a002ac
rasik <rasikkayasthav@gmail.com>
2024-06-17 19:51:37 +0545

    wait for element

--- autofill.py
+++ autofill.py
@@ -33,16 +33,18 @@ chrome_options= Options()
 def wait_for_element(driver, by,element_identifier,timeout=5):
     try:
         presentElement=EC.presence_of_element_located((by, element_identifier))
-        
+
     except TimeoutException:
         print(f"Timed out waiting for {element_identifier} to load")
+        return None
+    return driver.find_element(by,element_identifier)
 
 def set_fields(driver):
-    firstName_input = driver.find_element(By.ID,"jform_name")
-    userName_input = driver.find_element(By.ID,"jform_username")
-    password_input = driver.find_element(By.ID,"jform_password1")
-    conformPass_input = driver.find_element(By.ID,"jform_password2")
-    email_input = driver.find_element(By.ID,"jform_email1")
+    firstName_input = wait_for_element(driver,By.ID,"jform_name")
+    userName_input = wait_for_element(driver,By.ID,"jform_username")
+    password_input = wait_for_element(driver,By.ID,"jform_password1")
+    conformPass_input = wait_for_element(driver,By.ID,"jform_password2")
+    email_input = wait_for_element(driver,By.ID,"jform_email1")
 
     if firstName_input and userName_input and password_input and conformPass_input and email_input:
         firstName_input.send_keys(firstName)
@@ -52,7 +54,7 @@ def set_fields(driver):
         email_input.send_keys(email)
 
 def submit_click(driver):
-    submit_click = driver.find_element(By.XPATH,'//button[@type="submit" and @class="com-users-registration__register btn btn-primary validate"]')
+    submit_click = wait_for_element(driver,By.XPATH,'//button[@type="submit" and @class="com-users-registration__register btn btn-primary validate"]')
     print(submit_click)
     if submit_click:
         submit_click.click()
